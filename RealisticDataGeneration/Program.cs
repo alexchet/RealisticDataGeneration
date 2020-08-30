19624b01705fd77bd4fee7c80fe317a1edc1a9e3
Alex Chetcuti <chetcuti.alex@gmail.com>
2020-08-30 20:19:24 +0200

    Complex object and infinte data generation

--- RealisticDataGeneration/Program.cs
+++ RealisticDataGeneration/Program.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using Bogus;
+using Bogus.Extensions.Extras;
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace RealisticDataGeneration
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var paymentDetailsFaker = new Faker<PaymentDetails>()
+                .RuleFor(x => x.Amount, x => x.Finance.Amount())
+                .RuleFor(x => x.Currency, x => x.Finance.Currency().Code)
+                .RuleFor(x => x.CreditCardNumber, x => x.Finance.CreditCardNumberObfuscated());
+
+            var userFaker = new Faker<User>()
+                .RuleFor(x => x.Id, Guid.NewGuid)
+                .RuleFor(x => x.Name, x => x.Person.FullName)
+                .RuleFor(x => x.Email, x => x.Person.Email)
+                .RuleFor(x => x.Phone, x => x.Person.Phone)
+                .RuleFor(x => x.Address, x => x.Address.StreetAddress())
+                .RuleFor(x => x.City, x => x.Address.City())
+                .RuleFor(x => x.PostCode, x => x.Address.ZipCode())
+                .RuleFor(x => x.Country, x => x.Address.Country())
+                .RuleFor(x => x.PaymentDetails, x => paymentDetailsFaker);
+
+            foreach(var user in userFaker.GenerateForever())
+            {
+
+                var json = JsonSerializer.Serialize(user);
+                Console.WriteLine(json);
+                await Task.Delay(1000);
+            }
+            Console.ReadLine();
         }
     }
 }
---
+++ RealisticDataGeneration/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RealisticDataGeneration
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string PostCode { get; set; }
+        public string Country { get; set; }
+        public PaymentDetails PaymentDetails { get; set; }
+    }
+
+    public class PaymentDetails
+    {
+        public decimal Amount { get; set; }
+        public string Currency { get; set; }
+        public string CreditCardNumber { get; set; }
+    }
+}
